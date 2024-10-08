a6545049bb37a9b56b752c75f18d7f36d2f7cf75
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-08 09:46:07 +0530

    Add files via upload

---
+++ callback_problem.js
@@ -0,0 +1,31 @@
+﻿/*console.log("start");
+function imp(username) {
+    setTimeout(() => {
+
+        return `subbscribe to ${username}`;
+
+    }, 0);
+}
+
+const message = imp('Prashan verma');// to resolve this we have to use call back function.
+
+console.log(message);
+console.log("stop");*/
+
+/******* console.log("message");  gives undefined the reason behind this is   */
+
+console.log("start");
+function imp(username,callb) {
+    setTimeout(() => {
+
+        callb(`subscribe to ${username}`);
+
+    },1000);
+}
+
+const message = imp('Prashan verma',(message)=> {
+    console.log(message);
+});// to resolve this we have to use call back function.
+
+
+console.log("stop");// nnow this will give the output in correct way.
---
+++ promise.js
@@ -0,0 +1,18 @@
+﻿// syncronus code works in single threaded line
+console.log("hello");
+console.log("brother");
+console.log("kaise ho");//{hello;brother;Kaise ho}
+
+// Asyncronous code work as mulit threaded line and is always executed in the last.
+
+console.log("promise");
+
+setTimeout(() => {
+    console.log("Krta haii");
+}, 1000);/// this will work at last becouse this a async code.
+
+console.log("Hamesha");//{ppromise;hamesha;krta hai}
+
+
+
+   
